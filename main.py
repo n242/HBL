053b40937336dee6c78c085b259db10bdab90d20
Neta Oren <neta5128@gmail.com>
2023-06-11 12:45:12 +0300

    updated files

--- main.py
+++ main.py
@@ -2,73 +2,68 @@ from pyannote.audio import Pipeline, Audio
 
 import pandas as pd
 
-# from IPython.display import Audio as IPythonAudio
-# from pyannote.database.util import load_rttm
-# waveform, sr = Audio(mono="downmix").crop(DEMO_FILE, EXCERPT)
-# IPythonAudio(waveform.flatten(), rate=sr)
+from IPython.display import Audio as IPythonAudio
+from pyannote.database.util import load_rttm
 
-#
-# def with_ground_truth():
-#     OWN_FILE = {'audio': "combined_vid1.wav"}
-#
-#     # load audio waveform and play it
-#     waveform, sample_rate = Audio(mono="downmix")(OWN_FILE)
-#     IPythonAudio(data=waveform.squeeze(), rate=sample_rate, autoplay=True)
-#
-#     # loading ground truth
-#     groundtruths = load_rttm("groundtruth_marissa.rttm")
-#     # if OWN_FILE['audio'] in groundtruths:
-#     groundtruth = groundtruths[OWN_FILE['combined_vid1']]
-#     print("recodnized ground truth")
-#     # else:
-#     #     _, groundtruth = groundtruths.popitem()
-#     #     print("didnt recodnize ground truth")
-#
-#     pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization',
-#                                         use_auth_token="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")
-#
-#     diarization = pipeline(OWN_FILE, groundtruth)
-#
-#     # 5. print the result
-#     for turn, _, speaker in diarization.itertracks(yield_label=True):
-#         print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-#
-#     from pyannote.metrics.diarization import DiarizationErrorRate
-#     metric = DiarizationErrorRate()
-#     der = metric(groundtruth, diarization)
-#
-#     print(f'diarization error rate = {100 * der:.1f}%')
-#
-#
-# def new_ground_truth():
-#     from pyannote.database import FileFinder
-#
-#     # Define the path to your .rttm file
-#     rttm_file = '/path/to/your/ground_truth.rttm'
-#
-#     # Use FileFinder to load the .rttm file
-#     file_finder = FileFinder()
-#     annotation = file_finder(rttm_file)
-#     from pyannote.core import Segment, Annotation
-#
-#     # Create an empty Annotation
-#     ground_truth = Annotation()
-#
-#     # Iterate over the lines in the .rttm file
-#     with open(rttm_file, 'r') as f:
-#         for line in f:
-#             # Parse each line
-#             _, _, _, start, duration, _, _, speaker, _, _ = line.strip().split()
-#
-#             # Convert the start and duration to floats
-#             start = float(start)
-#             duration = float(duration)
-#
-#             # Create a Segment for the current speaker turn
-#             segment = Segment(start, start + duration)
-#
-#             # Add the speaker turn to the ground truth annotation
-#             ground_truth[segment] = speaker
+
+
+def with_ground_truth():
+    OWN_FILE = {'audio': "combined_vid1.wav"}
+
+    # load audio waveform and play it
+    waveform, sample_rate = Audio(mono="downmix")(OWN_FILE)
+    IPythonAudio(data=waveform.squeeze(), rate=sample_rate, autoplay=True)
+
+    # loading ground truth
+    groundtruths = load_rttm("groundtruth_marissa.rttm")
+    groundtruth = groundtruths[OWN_FILE['combined_vid1']]
+    print("recodnized ground truth")
+
+    pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization',
+                                        use_auth_token="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")
+
+    diarization = pipeline(OWN_FILE, groundtruth)
+
+    # 5. print the result
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+
+    from pyannote.metrics.diarization import DiarizationErrorRate
+    metric = DiarizationErrorRate()
+    der = metric(groundtruth, diarization)
+
+    print(f'diarization error rate = {100 * der:.1f}%')
+
+
+def new_ground_truth():
+    from pyannote.database import FileFinder
+
+    # Define the path to your .rttm file
+    rttm_file = '/path/to/your/ground_truth.rttm'
+
+    # Use FileFinder to load the .rttm file
+    file_finder = FileFinder()
+    annotation = file_finder(rttm_file)
+    from pyannote.core import Segment, Annotation
+
+    # Create an empty Annotation
+    ground_truth = Annotation()
+
+    # Iterate over the lines in the .rttm file
+    with open(rttm_file, 'r') as f:
+        for line in f:
+            # Parse each line
+            _, _, _, start, duration, _, _, speaker, _, _ = line.strip().split()
+
+            # Convert the start and duration to floats
+            start = float(start)
+            duration = float(duration)
+
+            # Create a Segment for the current speaker turn
+            segment = Segment(start, start + duration)
+
+            # Add the speaker turn to the ground truth annotation
+            ground_truth[segment] = speaker
 
 
 def test_anote(my_wav):
@@ -118,9 +113,9 @@ def using_visual_data(times, path):
 
 if __name__ == '__main__':
     #with_ground_truth()
-    time_speaker = test_anote("220_EMO_S.wav")
+    time_speaker = test_anote("data/220_EMO_S.wav")
     #time_speaker = test_anote("combined_vid1.wav")
-    path = '220_EMO_S.csv'
+    path = 'data/220_EMO_S.csv'
     #path = "test.csv"
     final_out = using_visual_data(time_speaker, path)
     print(final_out)
